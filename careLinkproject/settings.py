a47f2924b3a12047c02d2ec4abd0b798041ea8d1
Amado-Alpha <stanalphax@gmail.com>
2024-04-04 23:18:19 +0300

    third commit

--- careLinkproject/settings.py
+++ careLinkproject/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'careLinkproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bjBebabYrXtwusPWyHQLYJnaxeXZQNJT',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '57237',
     }
 }
 
